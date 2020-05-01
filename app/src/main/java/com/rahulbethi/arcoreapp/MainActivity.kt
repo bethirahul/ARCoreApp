33dcd212720bc96c3213e700ed39a3dbbfdce201
Rahul Bethi <rahulbethi@hotmail.com>
2020-05-01 03:55:16 -0700

    Added Models to Recycler View
    New Classes to handle Models

--- app/src/main/java/com/rahulbethi/arcoreapp/MainActivity.kt
+++ app/src/main/java/com/rahulbethi/arcoreapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,10 +12,27 @@ private const val BOTTOM_SHEET_PEEK_HEIGHT = 50f
 
 class MainActivity : AppCompatActivity() {
 
+    private val models = mutableListOf(
+        Model(R.drawable.chair,      "Chair",   R.raw.chair     ),
+        Model(R.drawable.desk,       "Desk",    R.raw.desk      ),
+        Model(R.drawable.garbagebin, "Bin",     R.raw.garbagebin),
+        Model(R.drawable.houseplant, "Plant",   R.raw.houseplant),
+        Model(R.drawable.lamp,       "Lamp",    R.raw.lamp      ),
+        Model(R.drawable.laptop,     "Laptop",  R.raw.laptop    ),
+        Model(R.drawable.printer,    "Printer", R.raw.printer   ),
+        Model(R.drawable.speaker,    "Speaker", R.raw.speaker   )
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupBottomSheet()
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        rvModels.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvModels.adapter = ModelAdapter(models)
     }
 
     private fun setupBottomSheet() {
